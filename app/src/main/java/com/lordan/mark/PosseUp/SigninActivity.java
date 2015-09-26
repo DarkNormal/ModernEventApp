d9ef20c0879f099180ad5eeed8168efda657318e
DarkNormal <mark.lordan@me.com>
2015-09-26 21:06:27 +0100

    TODO merge TextViews together for sign up to solve non-click area between method to make TextViews clickable Sign in branch created. master to remain working and runnable at all times

--- app/src/main/java/com/lordan/mark/PosseUp/SigninActivity.java
+++ app/src/main/java/com/lordan/mark/PosseUp/SigninActivity.java
@@ -17,16 +17,31 @@ public class SigninActivity extends AbstractActivity {
         setContentView(R.layout.signin_layout);
 
         TextView forgotPassword = (TextView) findViewById(R.id.forgot_password);
-        forgotPassword.setOnClickListener(new View.OnClickListener() {
-                                              @Override
-                                              public void onClick(View v) {
-                                                  int duration = Toast.LENGTH_SHORT;
-                                                  Context context = getApplicationContext();
-                                                  Toast toast = Toast.makeText(context, "Clicked!", duration);
-                                                  toast.show();
-                                              }
-                                          }
-        );
+        TextView noAccount = (TextView) findViewById(R.id.no_account);                  //TODO merge these two strings together as there is
+        TextView signUp = (TextView) findViewById(R.id.signup_text);                    //TODO a non clickable gap between them of 2-4dp
+        makeTextClickable(forgotPassword);
+        makeTextClickable(noAccount);
+        makeTextClickable(signUp);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        /*Override the back button action on sign in activity
+        * This prevents the return to StartActivity (logo screen)
+        */
+            moveTaskToBack(true);
+    }
+    public void makeTextClickable(TextView toClick){    //adds click listener to any TextViews within this activity
+        toClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int duration = Toast.LENGTH_SHORT;
+                Context context = getApplicationContext();
+                Toast toast = Toast.makeText(context, "Clicked!", duration);
+                toast.show();
+            }
+        });
 
     }
 
