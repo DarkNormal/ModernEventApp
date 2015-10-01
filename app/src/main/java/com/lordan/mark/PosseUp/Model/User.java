7ccd7cdf2b2ef2c023050e750f8f5086f21c4918
DarkNormal <mark.lordan@me.com>
2015-10-01 22:54:23 +0100

    Registration API using Futures and callbacks, no raw JSON or HTTP Account table in database with basic details Preliminary testing on creating accounts with duplicates

---
+++ app/src/main/java/com/lordan/mark/PosseUp/Model/User.java
@@ -0,0 +1,70 @@
+package com.lordan.mark.PosseUp.Model;
+
+import com.google.gson.annotations.SerializedName;
+/**
+ * Created by Mark on 10/1/2015.
+ */
+public class User {
+    @SerializedName("fullname")
+    private String fullname;
+
+    @SerializedName("email")
+    private String email;
+
+    @SerializedName("password")
+    private String password;
+
+    private String username;
+
+    @SerializedName("isRegistered")
+    private boolean isRegistered;
+
+    public User(){
+
+    }
+    public User(String email, String password, String fullname){
+        this.fullname = fullname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isRegistered() {
+        return isRegistered;
+    }
+
+    public void setIsRegistered(boolean isRegistered) {
+        this.isRegistered = isRegistered;
+    }
+}
--- app/src/main/java/com/lordan/mark/PosseUp/RegisterActivity.java
+++ app/src/main/java/com/lordan/mark/PosseUp/RegisterActivity.java
@@ -1,16 +1,29 @@
 package com.lordan.mark.PosseUp;
 
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.lordan.mark.PosseUp.Model.User;
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+
+import java.net.MalformedURLException;
+
 /**
  * Created by Mark on 9/30/2015.
  */
 public class RegisterActivity extends AbstractActivity{
+
+    private MobileServiceClient mobileServiceClient;
+    private ProgressDialog mProgressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,8 +57,51 @@ public class RegisterActivity extends AbstractActivity{
         }
         if(!isValidPassword(password.getText().toString())){
             password.setError("Invalid Password");
-
         }
+        else{
+            User newUser = new User(email.getText().toString(), password.getText().toString(), fullName.getText().toString());
+            boolean connected = true;
+            try {
+                mobileServiceClient = new MobileServiceClient(
+                        "https://markrecipe.azure-mobile.net/",
+                        "PARUUkHfFBFlppgVDdVjorhICtRehq99",
+                        this);
+                // .withFilter(new ProgressFilter());
+            } catch (MalformedURLException e) {
+                System.out.println(e.getMessage());
+                connected = false;
+            }
+            if(connected){
+                mProgressDialog = ProgressDialog.show(this, "dialog title",
+                        "dialog message", true);
+
+
+                ListenableFuture<User> result = mobileServiceClient.invokeApi( "register", newUser, User.class );
+
+                Futures.addCallback(result, new FutureCallback<User>() {
+                    @Override
+                    public void onFailure(Throwable exc) {
+                        createAndShowDialog((Exception) exc, "Error");
+                    }
+
+                    @Override
+                    public void onSuccess(User result) {
+                        //createAndShowDialog(result.isRegistered() + " is registered", "Completed Items");
+                        mProgressDialog.dismiss();
+                    }
+                });
+            }
+            }
+        }
+    private void createAndShowDialog(Exception e, String title){
+        createAndShowDialog(e.toString(), title);
+    }
+    private void createAndShowDialog(String message, String title){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage(message);
+        builder.setTitle(title);
+        builder.create().show();
+    }
     }
 
-}
