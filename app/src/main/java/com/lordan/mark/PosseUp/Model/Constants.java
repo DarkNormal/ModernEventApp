9ea20dfcdd7c1464f9605a6c44f9a5267bba4acd
DarkNormal <mark.lordan@me.com>
2015-10-11 15:56:36 +0100

    Documentation being added

--- app/src/main/java/com/lordan/mark/PosseUp/Model/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.lordan.mark.PosseUp.Model;
-
-/**
- * Created by Mark on 10/4/2015.
- */
-public final class Constants {
-    public static String MOBILE_SERVICE_URL = "https://posseup.azure-mobile.net/";
-    public static String MOBILE_SERVICE_APPLICATION_KEY = "fUHjIzptoMsHlnEWExsWBIUsLyxgvo85";
-}
