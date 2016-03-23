5b652060709d8717aa696988f09d44838f8e79e2
DarkNormal <mark.lordan@me.com>
2016-03-23 16:24:35 +0000

    UI fix to upcoming and past events, custom recyclerview adapter used to provide dividers Swipe to refresh

---
+++ PosseUpApp/app/src/main/java/com/lordan/mark/PosseUp/SimpleAdapter.java
@@ -0,0 +1,84 @@
+package com.lordan.mark.PosseUp;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.lordan.mark.PosseUp.Model.Event;
+import com.lordan.mark.PosseUp.UI.MainActivityGroup.CustomItemClickListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Mark on 23/03/2016.
+ */
+public class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.SimpleViewHolder> {
+
+    private final Context mContext;
+    private List<Event> mData;
+    private CustomItemClickListener listener;
+
+    public void add(Event e,int position) {
+        position = position == -1 ? getItemCount()  : position;
+        mData.add(position,e);
+        notifyItemInserted(position);
+    }
+
+    public void remove(int position){
+        if (position < getItemCount()  ) {
+            mData.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+
+    public static class SimpleViewHolder extends RecyclerView.ViewHolder {
+        public final TextView title;
+
+        public SimpleViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.recycler_event_title);
+        }
+    }
+
+    public SimpleAdapter(Context context, List<Event> data, CustomItemClickListener listener) {
+        mContext = context;
+        this.listener = listener;
+        if (data != null)
+            mData = new ArrayList<>(data);
+        else mData = new ArrayList<Event>();
+    }
+
+    public SimpleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final View view = LayoutInflater.from(mContext).inflate(R.layout.text_row_item, parent, false);
+        final SimpleViewHolder mViewHolder = new SimpleViewHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onItemClick(v, mViewHolder.getAdapterPosition());
+            }
+        });
+        return mViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(SimpleViewHolder holder, final int position) {
+        holder.title.setText(mData.get(position).getEventName());
+        holder.title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext,"Position ="+position,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+}
--- PosseUpApp/app/src/main/java/com/lordan/mark/PosseUp/UI/MainActivityGroup/Tab3.java
+++ PosseUpApp/app/src/main/java/com/lordan/mark/PosseUp/UI/MainActivityGroup/Tab3.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -26,6 +27,9 @@ import com.lordan.mark.PosseUp.CustomAdapter;
 import com.lordan.mark.PosseUp.Model.Constants;
 import com.lordan.mark.PosseUp.Model.Event;
 import com.lordan.mark.PosseUp.R;
+import com.lordan.mark.PosseUp.SimpleAdapter;
+import com.lordan.mark.PosseUp.SimpleSectionedRecyclerViewAdapter;
+import com.lordan.mark.PosseUp.UI.DividerItemDecoration;
 import com.lordan.mark.PosseUp.UI.EventDetailGroup.EventDetailsActivity;
 
 import org.json.JSONArray;
@@ -39,44 +43,33 @@ import java.util.List;
 
 public class Tab3 extends Fragment {
 
-    private List<Event> pastDataset, upcomingDataset;
+    private List<Event> eventDataSet, pastDataSet, upcomingDataSet;
     private String username;
     private final String TAG = "TAB3";
     private RequestQueue queue;
-    private CustomAdapter pastAdapter, upcomingAdapter;
+    private RecyclerView eventRecycler;
+    private SimpleAdapter mAdapter;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private List<SimpleSectionedRecyclerViewAdapter.Section> sections;
+    private View v;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.tab_3,container,false);
-        RecyclerView pastRecyclerView = (RecyclerView) v.findViewById(R.id.pastEventsRecycler);
-        RecyclerView upcomingRecyclerView = (RecyclerView) v.findViewById(R.id.upcomingEventsRecycler);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
-        LinearLayoutManager nextLayoutManager = new LinearLayoutManager(getContext());
+        v = inflater.inflate(R.layout.tab_3,container,false);
 
-        nextLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-
-        pastRecyclerView.setLayoutManager(layoutManager);
-        upcomingRecyclerView.setLayoutManager(nextLayoutManager);
-
-
-
-
-        pastAdapter = new CustomAdapter(getContext(), pastDataset, new CustomItemClickListener() {
+        eventRecycler = (RecyclerView) v.findViewById(R.id.eventsRecycler);
+        eventRecycler.setHasFixedSize(true);
+        eventRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        eventRecycler.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL));
+        sections = new ArrayList<>();
+        mSwipeRefreshLayout =(SwipeRefreshLayout) v.findViewById(R.id.event_list_swipe);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onItemClick(View v, int position) {
-                onEventClick(pastDataset, position);
+            public void onRefresh() {
+                // Refresh items
+                getEvents();
             }
         });
-        pastRecyclerView.setAdapter(pastAdapter);
-        upcomingAdapter = new CustomAdapter(getContext(), upcomingDataset, new CustomItemClickListener() {
-            @Override
-            public void onItemClick(View v, int position) {
-                onEventClick(upcomingDataset, position);
-            }
-        });
-        upcomingRecyclerView.setAdapter(upcomingAdapter);
-
         return v;
     }
     private void getEvents(){
@@ -85,22 +78,56 @@ public class Tab3 extends Fragment {
             public void onSuccess(JSONObject result) {
                 try {
                     Gson gson = new Gson();
+                    pastDataSet.clear();
+                    upcomingDataSet.clear();
                     for (int i = 0; i < result.getJSONArray("Events").length(); i++) {
 
                         Event e = gson.fromJson(result.getJSONArray("Events").getJSONObject(i).toString(), Event.class);
                         Calendar now = Calendar.getInstance();
                         if(e.getTime().after(now)){
                             Log.i(TAG, "event in the future");
-                            upcomingDataset.add(gson.fromJson(result.getJSONArray("Events").getJSONObject(i).toString(), Event.class));
+                            upcomingDataSet.add(gson.fromJson(result.getJSONArray("Events").getJSONObject(i).toString(), Event.class));
                         }
                         else{
                             Log.i(TAG, "event in the past");
-                            pastDataset.add(gson.fromJson(result.getJSONArray("Events").getJSONObject(i).toString(), Event.class));
+                            pastDataSet.add(gson.fromJson(result.getJSONArray("Events").getJSONObject(i).toString(), Event.class));
                         }
 
                     }
-                    pastAdapter.notifyDataSetChanged();
-                    upcomingAdapter.notifyDataSetChanged();
+                    eventDataSet.clear();
+
+                    eventDataSet.addAll(upcomingDataSet);
+                    sections.clear();
+                    //Sections
+                    sections.add(new SimpleSectionedRecyclerViewAdapter.Section(0,"UPCOMING"));
+                    sections.add(new SimpleSectionedRecyclerViewAdapter.Section(eventDataSet.size(),"PAST"));
+                    eventDataSet.addAll(pastDataSet);
+
+                    mAdapter = new SimpleAdapter(getContext(), eventDataSet, new CustomItemClickListener() {
+                        @Override
+                        public void onItemClick(View v, int position) {
+                            if(position > upcomingDataSet.size()){
+                                position -= 2;
+                            }
+                            else position -=1;
+                            Intent intent = new Intent(getContext(), EventDetailsActivity.class);
+                            intent.putExtra("EventID", eventDataSet.get(position).getEventID());
+                            try {
+                                intent.putExtra("EventLat", eventDataSet.get(position).getPlaceDetails().getVenueLocation().latitude);
+                                intent.putExtra("EventLng", eventDataSet.get(position).getPlaceDetails().getVenueLocation().longitude);
+                            }
+                            catch(NullPointerException npe){
+                                Log.e(TAG, "Place Details null");
+                            }
+                            startActivity(intent);
+                            Toast.makeText(getContext(), "clicked position: " + position, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    SimpleSectionedRecyclerViewAdapter.Section[] dummy = new SimpleSectionedRecyclerViewAdapter.Section[sections.size()];
+                    SimpleSectionedRecyclerViewAdapter mSectionedAdapter = new
+                            SimpleSectionedRecyclerViewAdapter(getContext(),R.layout.section,R.id.section_text,mAdapter);
+                    mSectionedAdapter.setSections(sections.toArray(dummy));
+                    eventRecycler.setAdapter(mSectionedAdapter);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -113,12 +140,14 @@ public class Tab3 extends Fragment {
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
+                mSwipeRefreshLayout.setRefreshing(false);
                 callback.onSuccess(response);
                 Log.i(TAG, "got response");
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                mSwipeRefreshLayout.setRefreshing(false);
                 Log.e(TAG, "got error");
 
             }
@@ -141,8 +170,9 @@ public class Tab3 extends Fragment {
     }
 
     private void initDataset() {
-        pastDataset = new ArrayList<>();
-        upcomingDataset = new ArrayList<>();
+        eventDataSet = new ArrayList<>();
+        upcomingDataSet = new ArrayList<>();
+        pastDataSet = new ArrayList<>();
         getEvents();
     }
 
