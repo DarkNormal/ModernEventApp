1f0d6816cb18355849e39df0c3b291a50a59c6a1
DarkNormal <mark.lordan@me.com>
2016-04-26 01:18:21 +0100

    Kotlin

---
+++ PosseUpApp/app/src/androidTest/java/com/lordan/mark/PosseUp/MainActivityTest.kt
@@ -0,0 +1,35 @@
+package com.lordan.mark.PosseUp
+
+import android.os.SystemClock
+
+import android.support.test.espresso.ViewAssertion
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.view.View
+
+import com.lordan.mark.PosseUp.UI.MainActivityGroup.MainActivity
+
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import java.util.concurrent.TimeUnit
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withText
+
+/**
+ * Created by Mark on 26/04/2016.
+ */
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @Rule @JvmField
+    public val main = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    @Test
+    fun launchMainActivity() {
+        onView(withText("Classic car show")).check(ViewAssertions.matches(isDisplayed()))
+    }
+}
---
+++ PosseUpApp/app/src/androidTest/java/com/lordan/mark/PosseUp/SignInActivityTest.kt
@@ -0,0 +1,30 @@
+package com.lordan.mark.PosseUp
+
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+
+import com.lordan.mark.PosseUp.UI.MainActivityGroup.MainActivity
+import com.lordan.mark.PosseUp.UI.SigninGroup.SigninActivity
+
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withText
+
+/**
+ * Created by Mark on 26/04/2016.
+ */
+@RunWith(AndroidJUnit4::class)
+class SignInActivityTest {
+    @Rule @JvmField
+    public val main = ActivityTestRule(SigninActivity::class.java)
+
+    @Test
+    fun launchMainActivity() {
+        onView(withText("Posse Up")).check(ViewAssertions.matches(isDisplayed()))
+    }
+}
